2406cf7bf3125d9f19c52ce98256b4c042848f11
root <root@localhost.localdomain>
2025-04-19 19:27:12 +0900

    going

--- src/app/sampletab/sub1/page.tsx
+++ src/app/sampletab/sub1/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const page = () => {
-  return <div>서브페이지 1</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center">
+        <span>무슨 페이지인지 모르겠다</span>
+        <span>무슨 페이지인지 모르겠다</span>
+      </div>
+    </div>
+  );
 };
 
 export default page;
