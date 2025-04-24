55530f1e0df1a2cbf8cd0b1e4379c1c71d685615
root <root@localhost.localdomain>
2025-04-24 12:24:26 +0900

    going

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -75,6 +75,15 @@ export default function Sidebar() {
               LOGIN
             </div>
           </div>
+          {/* 심심해서 만든 애플 */}
+          <div
+            className="flex justify-center items-center gap-2 p-3 cursor-pointer hover:bg-black/20"
+            onClick={() => router.push("/apple")}
+          >
+            <div className="text-[#1e1e1e] text-[16px] font-bold">
+              APPLE
+            </div>
+          </div>
         </div>
       </div>
     </div>
