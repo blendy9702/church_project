6dd9e7a0e57e8fcf2ddee03bc1fa98d3c7f28305
root <lch99206@naver.com>
2025-05-17 16:33:54 +0900

    class

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
@@ -12,27 +15,43 @@ export default function LoginPage() {
               placeholder="email@email.com"
             />
           </div>
-          <div className="mb-6">
-            <label className="block text-[#344054]">비밀번호</label>
+          <div className="mb-8">
+            <div className="flex justify-between">
+              <label className="block text-[#344054]">비밀번호</label>
+              <Link href="/login" className="text-[#5FB995]">
+                비밀번호 찾기
+              </Link>
+            </div>
             <input
               type="password"
               className="mt-1 w-full rounded-md border px-[16px] py-[12px] border-[#D0D5DD] hover:border-[#D1E9FF] focus:border-[#D1E9FF] focus:outline-none focus:ring-2 focus:ring-[#D1E9FF] placeholder:text-[#98A2B3] placeholder:text-[14px]"
               placeholder="비밀번호를 입력하세요."
             />
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-4">
             <button
               type="submit"
-              className="w-full rounded-md bg-[#5FB995] px-4 py-2 text-white hover:bg-[#49af86] cursor-pointer"
+              className="w-full rounded-md bg-[#5FB995] px-4 py-2 hover:bg-[#49af86] cursor-pointer"
             >
-              로그인
+              <span className="text-white font-[700]">로그인</span>
             </button>
             <button
               type="submit"
-              className="w-full rounded-md bg-[#5FB995] px-4 py-2 text-white hover:bg-[#49af86] cursor-pointer"
+              className="flex justify-center items-center w-full rounded-md bg-[#D1FFE9] px-4 py-2 hover:bg-[#96ebc9] cursor-pointer"
             >
-              회원가입
+              <div className="flex gap-2">
+                <Image src="/google.svg" alt="구글" width={20} height={20} />
+                <span className="text-[#5FB995] font-[700]">
+                  구글 계정으로 가입하기
+                </span>
+              </div>
             </button>
+            <div className="flex justify-center items-center gap-2">
+              <span className="text-[#98A2B3]">계정이 없으신가요?</span>
+              <Link href="/login" className="text-[#5FB995]">
+                가입하기
+              </Link>
+            </div>
           </div>
         </form>
       </div>
