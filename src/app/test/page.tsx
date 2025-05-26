ee3b94280df2322e886786ccb4fdf2360d3dd573
root <lch99206@naver.com>
2025-05-26 13:33:55 +0900

    mee

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,7 +6,9 @@ export default function TestPage() {
   return (
     <div>
       <div className="w-full h-screen flex flex-col items-center justify-center gap-10">
-        <span className="text-[20px] font-bold">안녕하세요</span>
+        <span className="text-6xl font-extrabold bg-gradient-to-r from-red-400 via-yellow-400 to-green-400 bg-clip-text text-transparent animate-bounce">
+          준영님, 안녕하세요! 🎉
+        </span>
         <Image src="/google.svg" alt="logo" width={100} height={100} />
       </div>
     </div>
